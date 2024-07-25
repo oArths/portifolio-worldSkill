b053fe225cdaa92a095971224b92552c00d50a03
Arthur <arthurferreira1417@gmail.com>
2024-07-25 20:50:13 -0300

    Fix: adicioando analitcspeed ao porjet

--- portifolio/src/index.js
+++ portifolio/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './page/home/index';
 import reportWebVitals from './reportWebVitals';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +12,7 @@ root.render(
   <React.StrictMode>
     <Home />
     <Analytics />
+    <SpeedInsights />
     </React.StrictMode>
 );
 
