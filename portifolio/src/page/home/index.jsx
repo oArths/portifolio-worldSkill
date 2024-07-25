84ea6d78abdf00f6b41494c666dfec65faa5e954
Arthur <arthurferreira1417@gmail.com>
2024-07-25 16:43:06 -0300

    Faet: criando pagina de sobre com as informações doartista e envio de forms

--- portifolio/src/page/home/index.jsx
+++ portifolio/src/page/home/index.jsx
@@ -48,8 +48,7 @@ export default function Home() {
               Projetos de Artístico
             </div>
             <div className={style.portsubtitle}>
-              Artístico é um artista consolidado na industria tendo partipação em diversas obras se destacando no cartoon
-
+            Artístico é um artista renomado na indústria, com destaque em trabalhos de cartoon.
             </div>
           </div>
         </div>
@@ -65,8 +64,8 @@ export default function Home() {
                   Coca-Cola
                 </div>
                 <div className={style.CardDescri}>
-                  um parceria memoravel com a Coca-Cola onde ele desenvolverl ilustrações pra mesma em seu comercial na copa do mundo
-                  onde ficou responsavel por desenvolver novos botoms para serem brindes da coca-cola na copa do mundo.
+                  Uma parceria com a Coca-Cola para criar ilustrações para um comercial da Copa do Mundo
+                  e desenvolver novos designs de botões para brindes.
                 </div>
               </div>
             </div>
@@ -79,8 +78,7 @@ export default function Home() {
                   Outubro Rosa
                 </div>
                 <div className={style.CardDescri}>
-                  um parceria com o governo brasileiro onde participou da divulgação do mês  de conciencia ao cancer de mama desenvolvendo ilustrações de mulheres
-                </div>
+                  Participou de uma campanha do governo brasileiro para conscientização sobre o câncer de mama, criando ilustrações que valorizam a mulher.                </div>
               </div>
             </div>
             <div className={style.CardProjwoman}>
@@ -92,14 +90,53 @@ export default function Home() {
                   Vegan-Food
                 </div>
                 <div className={style.CardDescri}>
-                  Artístico participou da campanha de lançamento da Vegan-Food, Empresa de fast-food totalmente vegana, sendo a principal mente criativa na idealização da marca e seu desing
-                </div>
+                  Artístico foi o principal designer da Vegan-Food, uma nova rede de fast-food totalmente vegana, responsável por criar a identidade visual da marca desde o início                </div>
               </div>
             </div>
           </div>
 
         </div>
 
+      </div>
+      <div id="sobre" className={style.Sobre}>
+        <div className={style.SobretextCoatiner}>
+          <div className={style.TextSobreCopnatiner}>
+            <div className={style.SobreTitle}>
+              Artístico
+            </div>
+            <div className={style.SobreSubtitle}>
+              Artístico é um ilustrador talentoso com um portfólio diversificado com trabalhos editoriais e publicitários
+              mais sofisticados.
+              Com um estilo único e versátil, ele utiliza diversas técnicas, como aquarela e ilustração digital,
+              para criar obras de arte que encantam e inspiram.
+            </div>
+          </div>
+        </div>
+        <div className={style.requestConatiner}>
+          <div className={style.InputConatiner}>
+            <div className={style.cardinput}>
+              <div className={style.infoSobre}>
+                Tire suas Duvidas
+
+              </div>
+              <input className={style.inputSobre} type="text" placeholder="Nome" />
+              <textarea className={style.TextArea} placeholder="Asssunto" maxLength={600} resize="none" rows="10"></textarea>
+              <div className={style.ConainerSobreButton}>
+                <div className={style.Sobrebutton}>
+                  Enviar
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className={style.footCont}>
+        <div className={style.textfoof}>
+        Todos os direitos reservados © Artístico
+
+        </div>
       </div>
 
     </div>
