ea88a38a5720e0cb4845637580a853f949416782
Arthur <arthurferreira1417@gmail.com>
2024-07-25 18:14:40 -0300

    Faet: adicioando vercel logs

--- portifolio/src/index.js
+++ portifolio/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './page/home/index';
 import reportWebVitals from './reportWebVitals';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home />
+    <SpeedInsights />
   </React.StrictMode>
 );
 
